994d13e6e39ba2cae43399ae43e2670881e551d4
Khrystyna Rymar <>
2020-01-09 18:36:05 +0200

    added some js

---
+++ main.js
@@ -0,0 +1,12 @@
+var age=+prompt("Enter your age");
+var gender = prompt("Enter your gender");
+
+if(age>34 && gender==="female"){
+    alert("You are woman older then 34 years");
+}else if( age<34 && gender==="female"){
+    alert("You are woman younger then 34 years");
+}else if( age>34 && gender==="male"){
+    alert("You are man older then 34 years");
+}else if( age<34 && gender==="male"){
+    alert("You are man younger then 34 years");
+}
