d848fe2097c8960098e89cdf25553e4927dfea11
Khrystyna Rymar <>
2020-01-11 12:37:10 +0200

    added some logic

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-var age=+prompt("Enter your age");
-var gender = prompt("Enter your gender");
+let allUsers = [];
 
-if(age>34 && gender==="female"){
-    alert("You are woman older then 34 years");
-}else if( age<34 && gender==="female"){
-    alert("You are woman younger then 34 years");
-}else if( age>34 && gender==="male"){
-    alert("You are man older then 34 years");
-}else if( age<34 && gender==="male"){
-    alert("You are man younger then 34 years");
+function saveUser() {
+  let user ={};
+  let fullName = document.getElementById("fullName").value;
+  user.name = fullName;
+  let age = document.getElementById("age").value;
+  user.age = age;
+  let phone = document.getElementById("phoneNumber").value;
+  user.phoneNumber = phone;
+  let email = document.getElementById("email").value;
+  user.email = email;
+  let city = document.getElementById("city").value;
+  user.city = city;
+  let postCode = document.getElementById("postCode").value;
+  user.post = postCode;
+  allUsers.push(user);
+  emptyAllFields();
+}
+function emptyAllFields(){
+    document.getElementById("fullName").value ="";
+    document.getElementById("age").value ="";
+    document.getElementById("phoneNumber").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("city").value = "";
+    document.getElementById("postCode").value ="";
 }
