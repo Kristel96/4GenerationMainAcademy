cb54dc8f253a97d701d23dc13bfdc71a09e39f22
Khrystyna Rymar <>
2020-01-24 12:40:03 +0200

    aded some logic

--- main.js
+++ main.js
@@ -1,31 +1,44 @@
-let allUsers = [];
+let allArticles = [];
+function saveArticle() {
+    let articleObj = {};
+ let title = document.getElementById("title").value;
+ let imgUrl = document.getElementById("imgsrc").value;
+ let article = document.getElementById("text").value;
+ if(title==="" || imgUrl==="" || article==="") {
+     alert("All inputs should not be empty")
+ }else {
+     articleObj.title =title;
+     articleObj.image = imgUrl;
+     articleObj.article = article;
+     articleObj.sentences = article.split(".");
+     articleObj.sentencesNum = articleObj.sentences.length;
+     allArticles.push(articleObj);
+     let info = document.getElementById("info");
+     info.innerText = "The article has been saved";
 
-function saveUser() {
-  let user ={};
-  let fullName = document.getElementById("fullName").value;
-  user.name = fullName;
-  let age = document.getElementById("age").value;
-  user.age = age;
-  let phone = document.getElementById("phoneNumber").value;
-  user.phoneNumber = phone;
-  let email = document.getElementById("email").value;
-  user.email = email;
-  let city = document.getElementById("city").value;
-  user.city = city;
-  let postCode = document.getElementById("postCode").value;
-  user.post = postCode;
-  allUsers.push(user);
-  emptyAllFields();
-}
-function emptyAllFields(){
-    document.getElementById("fullName").value ="";
-    document.getElementById("age").value ="";
-    document.getElementById("phoneNumber").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("city").value = "";
-    document.getElementById("postCode").value ="";
-}
+     emptyFields();
+     console.dir(allArticles);
+ }
 
-var str = "Fred and Bob and Tod";
-var arr = str.split('and');
- console.log(arr);
+ //  this logic can be changed later
+
+}
+function emptyFields(){
+   document.getElementById("title").value ="";
+   document.getElementById("imgsrc").value = "";
+   document.getElementById("text").value = "";
+
+    setTimeout(function () {
+        document.getElementById("info").innerText = ""
+    },4000);
+
+}
+function showArticles(){
+ for(var i=0;i<allArticles.length;i++){
+     let objContainer = document.createElement("article");
+     let obj = allArticles[i];
+     let text  = document.createTextNode(obj.title);
+     objContainer.appendChild(text);
+    document.getElementById("articleContainer").appendChild(objContainer);
+ }
+}
