589098e475f9ac18c3bb2e9ad9ca4ebe57f5ac32
Khrystyna Rymar <>
2020-02-06 20:01:33 +0200

    min max and prompt

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
-let a = Math.random();
-console.log(a);
+// let name = prompt("What is your name?");
+// let age = prompt("What is your age");
+// let text = `Hello ${name}. Your age is ${age}`;
+//  prompt(text);
+//
+
+
+// let input = +prompt("please enter some figure");
+// if(input>0){
+//     alert("the figure is possitive");
+// } else {
+//     alert("the figure is negative");
+// }
+
+
+
+// Find the max and min in array
+// let arr = [34,-23,11,1,8,29,4,6,-3];
+//
+// let min = arr[0];
+// let max = arr[0];
+//
+// for (let i=0; i<arr.length; i++){
+//     if (min > arr[i+1]){
+//         min =arr[i+1]
+//     }else if(max<arr[i+1]){
+//         max =arr[i+1];
+//     }
+// }
+// console.log(max);
+// console.log(min);
+
+
