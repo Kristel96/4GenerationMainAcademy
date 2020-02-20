0c619349f895a4e8339d43fccd0e40c0336b3d17
Khrystyna Rymar <>
2020-02-20 21:34:30 +0200

    added Post

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
     let mainWrap = document.getElementById("mainWraper");
     let userWrap = document.getElementById("userInfoWrap");
     getAllUsers(1);
-
+    sendData();
 
 
 
@@ -18,6 +18,22 @@ function getData(url, callback){
     };
     xhttp.open("GET", url, true);
     xhttp.send();
+}
+    // https://reqres.in/api/users
+function sendData(){
+
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 201) {
+              let a =  JSON.parse(this.responseText);
+                console.dir(a);
+            }
+        };
+        xhttp.open("POST", "https://reqres.in/api/users", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send("fname=Henry&lname=Ford");
+
+
 }
 
  function getAllUsers(pageId) {
