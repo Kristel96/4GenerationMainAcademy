8bbd766444f9965de23cb2b8b20799be1c0410e3
Khrystyna Rymar <>
2020-03-11 20:46:02 +0200

    added require files

--- main.js
+++ main.js
@@ -2,15 +2,29 @@ function register(){
     let users = [];
     let user ={};
 
-    let email = document.getElementById("email");
-    let fullneme = document.getElementById("fullname");
-    let pass = document.getElementById("password");
-    let confpass = document.getElementById("confirmpass");
-    if (pass === confpass) {
-        user.email = email;
-        user.fullname = fullneme;
-        user.password = pass;
-        users.push(user);
+    let fullname = document.getElementById("fullname").value;
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("password").value;
+    let confpass = document.getElementById("confirmpass").value;
+    if (fullname === "" && email === "" && pass === "" && confpass ===""){
+        alert("Input Should Not be empty!")
+    }else{
+        if (pass === confpass) {
+            user.email = email;
+            user.fullname = fullname;
+            user.password = pass;
+            users.push(user);
+            localStorage.setItem("users", JSON.stringify(users));
+            location.href = "login.html";
+        }else {
+            document.getElementById("errorField").innerHTML = "The passwords do no match!";
+            setTimeout(function () {
+                document.getElementById("errorField").innerText="";
+            }, 4000);
+        }
     }
 
 }
+let storedUsers = JSON.parse(localStorage.getItem("users"));
+
+//location href --- to redirect pages
